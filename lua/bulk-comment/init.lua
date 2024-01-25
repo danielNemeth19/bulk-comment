012531eb0b39d55f75a419578496cce8a3d270a2
Daniel Nemeth <daniel.nemeth83@yahoo.com>
2024-01-25 23:34:05 +0100

    (feat) Getting position of cursor

--- lua/bulk-comment/init.lua
+++ lua/bulk-comment/init.lua
@@ -1,19 +1,27 @@
 local M = {}
 
+local commentMap = {
+	lua = "--",
+	python = "# ",
+	go = "//"
+}
+
 M.get_file_type = function ()
-	local myType = vim.bo.filetype
-	M['type'] = myType
-	if myType == "lua" then
-		print("this is a lua file")
-		M['comment_string'] = "--"
-	elseif myType == "python" then
-		print("this is a python file")
-		M['comment_string'] = "#"
+	local type = vim.bo.filetype
+	M['type'] = type
+	if commentMap[type] then
+		M['comment_string'] = commentMap[type]
+		print("type is ".. M["comment_string"])
+		local line = vim.api.nvim_get_current_line()
+		print("line is ".. line)
+		local pos = vim.fn.getpos('.')
+		local rn, col = pos[2], pos[3]
+		print("row ".. rn, "col: ".. col)
+	else
+		print("type is ".. type)
 	end
 end
 
 
-M.get_file_type()
-P(M)
 
 return M
--- plugin/loading.lua
+++ plugin/loading.lua
@@ -1,1 +1,4 @@
 print("bulk commenter plugin is loaded")
+local bc = require("bulk-comment")
+
+vim.keymap.set("n", "<C-q>", bc.get_file_type)
